9cabcffb3fa4e37086a962c91d428844f5a51d71
= <wwlane@umn.edu>
2015-07-22 21:17:55 -0500

    attempt at using afterRender to customize header

--- main.js
+++ main.js
@@ -15,9 +15,65 @@
   var table;
   table = new Handsontable(container, {
     data: data,
-    startRows: 8,
-    startCols: 10,
+    rowHeaders: function(row) {
+      return "<span style='background-color:green;'>one</span><span style='background-color:red'>two</span>";
+    },
+    colHeaders: function(col) {
+      var html = "";
+      switch(col) {
+        case 1:
+          html = "U.S.";
+          break;
+        case 5:
+          html = "Canada";
+          break;
+        case 8:
+          html = "Mexico";
+          break;
+        default:
+          html = "";
+          break;
+      }
+      return html;
+    },
+    autoColumnSize: true,
+    afterRenderer: function() {
+      var thecount = 0;
+      var totalNumCountries;
+      var countryElem;
+      var indexOfCountryElem;
+      var thsAfterCountryElem;
+      var allThs = $(".htCore tr:first th");
+      totalNumCountries = _.size(_.filter(allThs, function(item){ return $(item).text().trim() !== "" }));
+      var getIndexOfCountryElem = function(elem) {
+        return _.indexOf(allThs, elem)
+      }
+      if(thecount >= totalNumCountries) return;
+        allThs.each(function(item) {
+        var $th = $(this);
+        if($th.text().trim() !== "") {
+          countryElem = $th[0];
+          var index = getIndexOfCountryElem(countryElem);
+          var ths = allThs.slice(index);
+          var text = "";
+          var i = 1;
+          var total = 1;
+          while(text === "" && i < ths.length) {
+            var nextText = $(ths[i]).text().trim();
+            if(nextText === "") {
+              total += 1;
+              i += 1;
+              debugger
+              allThs.remove($(ths[i]));
+            }else {
+             text = "asdfasdf";
+            }
+          }
+          debugger
+          $(countryElem).attr('colspan', total);
+          thecount += 1;
+        }
+      });
+    }
   });
-
-  console.log("I'm actually running and doing something!");
 }());
